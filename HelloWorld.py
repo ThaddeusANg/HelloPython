abd4f4f4b048bea57b73680b4ad52b5f5eeb10b4
Thaddeus Ng <thaddeus.a.ng@gmail.com>
2018-08-23 23:06:11 -0700

    python lessons 1 and 2

---
+++ HelloWorld.py
@@ -0,0 +1,26 @@
+print("Just learning python please ignore")
+print("Lesson 1")
+print("Hello World")
+x = 1
+if x == 1:
+    print("x is 1")
+
+print("Goodbye World")
+
+print("Lesson 2 variables")
+test = "test"
+print(test+" is a string")
+
+floatingNum = 12.34
+print(floatingNum, "is a floating num")
+
+num = 1
+print(float(num), "is cast to a floating num")
+
+a,b = 3,4
+print(a,b, "are declared simultaneously and if separated by comma are treated as strings")
+
+sample = 15.5
+if isinstance(sample, float):
+    print(sample, "is of floating number type")
+    print("check instancetypes with isinstance(variable, type)")
